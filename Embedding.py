294389c12be96453e8b877c9a7d440821db57a98
CampingTurret <l.gaeta@student.tudelft.nl>
2023-12-13 09:06:17 +0100

    Word2Vec

--- Embedding.py
+++ Embedding.py
@@ -1,3 +1,43 @@
 import numpy as np
+import gensim
+import os
+import GeneralHelperFunctions
+import typing
+import pandas as pd
+
+class RestartableCurpus():
+
+    def __init__(self, corpus: typing.Generator) -> None:
+        """Corpus has to be a generator"""
+        GeneralHelperFunctions.safe_filtered_corpus(corpus)
+    
+    def __iter__(self) -> typing.Generator:
+        return GeneralHelperFunctions.load_filtered_corpus()
+        
+
+def Word2Vec(corpus: typing.Generator | None = None) -> gensim.models.Word2Vec:
+    W2V = gensim.models.Word2Vec(sentences=corpus, min_count= 20, window=8,vector_size=2000, epochs= 20, sg=1, workers= os.cpu_count()*2)
+    return W2V
 
 
+def SaveWord2Vec(W2V: gensim.models.Word2Vec):
+    W2V.save("W2V-Full.model")   
+    W2VLite = W2V.wv
+    W2VLite.save("W2V-Stripped.model")             
+
+
+def LoadWord2VecStripped() -> gensim.models.KeyedVectors:
+    return gensim.models.KeyedVectors.load("W2V-Stripped.model", mmap='r')
+
+def CheckProximityUnloaded(w1: list[float] | float, w2: list[float] | float) -> pd.DataFrame:
+    W2V = LoadWord2VecStripped()
+    if isinstance(w1, float):
+        w1 = [w1]
+    if isinstance(w2, float):
+        w2 = [w2]
+    arr = np.empty((len(w1), len(w2)), dtype=float)    
+    for i1, First in enumerate(w1):
+        arr[i1,:] = W2V.distances(First, w2)
+
+    return pd.DataFrame(arr, w2, w1)
+        
---
+++ GeneralHelperFunctions.py
@@ -0,0 +1,19 @@
+import typing
+
+def Gen2Corpus(gen: typing.Generator) -> list[str]:
+    corpus = []
+    for s in gen:
+        corpus.append(s)
+    return tuple(corpus)
+
+def safe_filtered_corpus(gen: typing.Generator) -> None:
+    f = open('SaveFilteredCorpus.txt', 'w')
+    for s in gen:
+        f.write(s + '\n')
+    f.close()
+
+def load_filtered_corpus() -> typing.Generator:
+    f = open('SaveFilteredCorpus.txt', 'r')
+    for l in f:
+        yield l.strip()
+    f.close()
